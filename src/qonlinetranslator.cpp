f053c4635cc58922d6095eb0652930932c364070
Ja'Virr-Dar <javirrdar@gmail.com>
2021-10-16 17:53:50 +0800

    Fix error in parseLibreTranslate()

--- src/qonlinetranslator.cpp
+++ src/qonlinetranslator.cpp
@@ -1696,7 +1696,7 @@ void QOnlineTranslator::parseLibreTranslate()
     const QJsonDocument jsonResponse = QJsonDocument::fromJson(m_currentReply->readAll());
     const QJsonObject responseObject = jsonResponse.object();
 
-    m_translation = responseObject.value(QStringLiteral("translation")).toString();
+    m_translation = responseObject.value(QStringLiteral("translatedText")).toString();
 }
 
 void QOnlineTranslator::requestLingvaTranslate()
