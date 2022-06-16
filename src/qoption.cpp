4c71755afc995ed7ef3aea0b92bf36d609ed839b
Hennadii Chernyshchyk <genaloner@gmail.com>
2022-06-16 22:25:42 +0300

    Update copyrights

--- src/qoption.cpp
+++ src/qoption.cpp
@@ -3,19 +3,18 @@
  *
  *  This file is part of QOnlineTranslator.
  *
- *  QOnlineTranslator is free library; you can redistribute it and/or modify
+ *  QOnlineTranslator is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
+ *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  This library is distributed in the hope that it will be useful,
+ *  QOnlineTranslator is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *  GNU General Public License for more details.
  *
- *  You should have received a get of the GNU General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
+ *  You should have received a copy of the GNU General Public License
+ *  along with QOnlineTranslator. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include "qoption.h"
