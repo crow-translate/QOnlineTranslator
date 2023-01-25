f3ce99e522e95b67550caec8c47291e4d873f186
Volk_Milit <javirrdar@gmail.com>
2023-01-25 23:18:28 +0800

    Use QJsonValueRef instead of QJsonValue in iterator

--- src/qonlinetranslator.cpp
+++ src/qonlinetranslator.cpp
@@ -1313,9 +1313,8 @@ void QOnlineTranslator::parseGoogleTranslate()
         for (const QJsonValueRef exampleData : jsonData.at(12).toArray()) {
             const QJsonArray exampleDataArray = exampleData.toArray();
             const QString typeOfSpeech = exampleDataArray.at(0).toString();
-            const QJsonArray examplesArray = exampleDataArray.at(1).toArray();
 
-            for (const QJsonValue &example : examplesArray) {
+            for (const QJsonValueRef example : exampleDataArray.at(1).toArray()) {
                 const QJsonArray exampleArray = example.toArray();
                 m_examples[typeOfSpeech].append({exampleArray.at(2).toString(), exampleArray.at(0).toString()});
             }
